53f22cbd0c3c5ab0b8ef4f09c10b0a320c3a7475
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2021-04-11 18:16:51 +0100

    Remove the Firefox hack

--- client.js
+++ client.js
@@ -1,5 +1,3 @@
-var cookies = require('./lib/cookies')
-
 module.exports = function(url, handle) {
 
   if (typeof url == 'function') {
@@ -87,50 +85,7 @@ module.exports = function(url, handle) {
       // When the host is localhost, this then results in the connection being
       // terminated after 30 seconds.
       disconnect()
-
-      // Fix for 30-second timeout/disconnection issue on loads from memory cache
-      // when Firefox is running on host localhost.
-      if (navigator.userAgent.includes('Firefox') && document.location.host === 'localhost') {
-        // Are we about to carry out a force reload? If so, flag it and also save the
-        // scroll location in a cookie so that we can restore it when the page reloads.
-        if (window.locationToRestoreAfterForcedReload !== undefined) {
-          cookies.add('pageWasForceReloaded')
-          cookies.add('scrollX', locationToRestoreAfterForcedReload.x)
-          cookies.add('scrollY', locationToRestoreAfterForcedReload.y)
-        }
-      }
     })
-
-    // Fix for 30-second timeout/disconnection issue on loads from memory cache
-    // when Firefox is running on host localhost.
-    if (navigator.userAgent.includes('Firefox') && document.location.host === 'localhost') {
-      // If a location was specified to restore after a forced reload, scroll to it.
-      if (cookies.exists('pageWasForceReloaded')) {
-        window.scroll(parseInt(cookies.value('scrollX')), parseInt(cookies.value('scrollY')))
-
-        // Delete the cookies.
-        cookies.remove('pageWasForceReloaded')
-        cookies.remove('scrollX')
-        cookies.remove('scrollY')
-      } else {
-        // If the page wasn’t force reloaded by us, it was manually reloaded by someone and
-        // they might have done a regular reload (e.g., hit the refresh button), in which case
-        // we must do a forced reload to be sure that we avoid the timeout/disconnection issue.
-        // Worst case scenario, this means that on Firefox running on host localhost, we do one
-        // unnecessary reload if the person did, in fact, do a manual force reload.
-        // I can live with that :)
-
-        // Save the current scroll position as Firefox will lose this after a forced reload
-        // but we can reset it manually.
-        window.locationToRestoreAfterForcedReload = {
-          x: window.scrollX,
-          y: window.scrollY
-        }
-
-        // Do a forced reload, skipping Firefox’s memory cache (and thus the timeout/disconnection issue).
-        location.reload(true)
-      }
-    }
   }
 
   if (!window.EventSource) init = createIframe
